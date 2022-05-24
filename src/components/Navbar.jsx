71940ace7b461183d14b6a38d701fdf90e5645e0
patriarch11 <o5zaichenko@gmail.com>
2022-05-24 16:49:04 +0300

    init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Nav = () => {
     return (
         <nav className='nav'>
-            <div><a href='#'>Main</a></div>
+            <div><a href='#'>Main 1</a></div>
             <div><a href='#'>Order</a></div>
             <div><a href='#'>Bonus points</a></div>
             <div><a href='#'>Account settings</a></div>
